b5c533b953c7020e833e443675b1b2b80d72fff7
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-06-14 09:21:57 +0700

    Result page

--- src/services/answersService.js
+++ src/services/answersService.js
@@ -4,3 +4,8 @@ export const getListAnswersByUserId = async (userId) => {
     const result = await get(`answers?userId=${userId}`);
     return result;
 }
+
+export const getAnswer = async (id) => {
+    const result = await get(`answers/${id}`);
+    return result;
+}
