062f7bee0efa1bc231a7cb7b00b4318975a822a9
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2026-03-26 21:01:46 +0700

    Topic management

--- server/api/v1/controller/topic.controller.js
+++ server/api/v1/controller/topic.controller.js
@@ -3,19 +3,21 @@ const Topic = require("../models/topic.model");
 // [GET] /api/v1/topics
 module.exports.index = async (req, res) => {
   try {
-    const topics = await Topic.find({
-      deleted: false
-    });
+    const keyword = req.query.keyword || "";
+    
+    let find = {
+      deleted: false,
+      name: { $regex: keyword, $options: "i" }
+    };
+
+    const topics = await Topic.find(find).sort({ createdAt: -1 });
 
     res.json({
       code: 200,
       data: topics
     });
   } catch (error) {
-    res.json({
-      code: 400,
-      message: error
-    });
+    res.status(400).json({ code: 400, message: error.message });
   }
 }
 
@@ -58,5 +60,72 @@ module.exports.infoTopic = async (req, res) => {
       code: 400,
       message: error
     });
+  }
+}
+
+// [POST] /api/v1/topics/create
+module.exports.create = async (req, res) => {
+  try {
+    const record = new Topic(req.body);
+    await record.save();
+
+    res.json({
+      code: 200,
+      message: "Tạo chủ đề thành công!",
+      data: record
+    });
+  } catch (error) {
+    res.status(400).json({ code: 400, message: "Lỗi khi tạo chủ đề!" });
+  }
+}
+
+// [PATCH] /api/v1/topics/update/:id
+module.exports.update = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Topic.updateOne({ _id: id }, req.body);
+
+    res.json({
+      code: 200,
+      message: "Cập nhật thành công!"
+    });
+  } catch (error) {
+    res.status(400).json({ code: 400, message: "Cập nhật thất bại!" });
+  }
+}
+
+// [DELETE] /api/v1/topics/delete/:id
+module.exports.delete = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Topic.updateOne({ _id: id }, {
+      deleted: true,
+      deletedAt: new Date()
+    });
+
+    res.json({
+      code: 200,
+      message: "Xóa chủ đề thành công!"
+    });
+  } catch (error) {
+    res.status(400).json({ code: 400, message: "Lỗi hệ thống khi xóa!" });
+  }
+}
+
+// [PATCH] /api/v1/topics/delete-many
+module.exports.deleteMany = async (req, res) => {
+  try {
+    const ids = req.body.ids;
+    await Topic.updateMany({ _id: { $in: ids } }, {
+      deleted: true,
+      deletedAt: new Date()
+    });
+
+    res.json({
+      code: 200,
+      message: "Xóa danh sách chủ đề thành công!"
+    });
+  } catch (error) {
+    res.status(400).json({ code: 400, message: "Lỗi khi xóa nhiều!" });
   }
 }
